52f245b3dcac8735af5c081d7231d151b74f184e
barbaracarvajal <barb.carvajalsaez@gmail.com>
2023-11-20 00:49:50 -0300

    ahora si q siii

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ dotenv.config();
 //database config
 connectDB();
 
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //rest object
 const app = express();
 
